ee19414a8b7ce1211bbcf3a909e770dfdef7fd40
max <maczejko.max@ya.ru>
2022-07-12 21:03:14 +0600

    new numbers

---
+++ les01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.WriteLine("Введите трехзначное число :");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+string Number = Convert.ToString(NumberA);
+Console.WriteLine("вторая цифра этого числа : "+Number[2]);
